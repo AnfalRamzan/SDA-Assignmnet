73d7bffe2cea748081eef8f2fec962f0c12f54b5
Anfal Ramzan <anfurajpootbhatti@gmail.com>
2026-04-13 10:52:48 +0500

    Refactor Library class for better design
    Refactor Library class to use default constructor and remove unnecessary fields and methods.

--- After/SRP/Library.java
+++ After/SRP/Library.java
@@ -2,8 +2,6 @@ package LMS;
 
 public class Library {
 
-    private String name;
-
     private BookManager bookManager;
     private PersonManager personManager;
     private SearchService searchService;
@@ -12,61 +10,18 @@ public class Library {
     private HistoryService historyService;
     private DatabaseService databaseService;
 
-    // Constructor Injection (BONUS - DIP)
-    public Library(
-            BookManager bookManager,
-            PersonManager personManager,
-            SearchService searchService,
-            FineService fineService,
-            LoginService loginService,
-            HistoryService historyService,
-            DatabaseService databaseService
-    ) {
-        this.bookManager = bookManager;
-        this.personManager = personManager;
-        this.searchService = searchService;
-        this.fineService = fineService;
-        this.loginService = loginService;
-        this.historyService = historyService;
-        this.databaseService = databaseService;
+    // ❌ BEFORE: Library itself creates objects (BAD DESIGN)
+    public Library() {
+        this.bookManager = new BookManager();
+        this.personManager = new PersonManager();
+        this.searchService = new SearchService();
+        this.fineService = new FineService();
+        this.loginService = new LoginService();
+        this.historyService = new HistoryService();
+        this.databaseService = new DatabaseService();
     }
-
-    // Library name
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLibraryName() {
-        return name;
-    }
-
-    // Delegate methods
 
     public void addBook(Book b) {
         bookManager.addBookinLibrary(b);
-    }
-
-    public void viewBooks() {
-        bookManager.viewAllBooks();
-    }
-
-    public void addBorrower(Borrower b) {
-        personManager.addBorrower(b);
-    }
-
-    public void addClerk(Clerk c) {
-        personManager.addClerk(c);
-    }
-
-    public Person login() {
-        return loginService.login();
-    }
-
-    public void viewHistory() {
-        historyService.viewHistory();
-    }
-
-    public double computeFine() {
-        return fineService.computeFine();
     }
 }
